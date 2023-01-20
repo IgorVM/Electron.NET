d0943da8b4ab2b98236fcfc2becd10318b99f3b5
Igor_Morozov <imorozov@forefrontcorp.com>
2023-01-21 00:55:02 +0500

    Updated version in buildReleaseNuGetPackages.sh

--- buildReleaseNuGetPackages.sh
+++ buildReleaseNuGetPackages.sh
@@ -1,15 +1,14 @@
 echo "Start building Electron.NET dev stack..."
-
+# Set the Electron.Net Version Number
+ENETVER=19.1.6
 echo "Restore & Build API"
 cd ElectronNET.API && \
 dotnet restore && \
-dotnet build --configuration Release --force /property:Version=5.22.12 && \
-dotnet pack /p:Version=5.22.12 --configuration Release --force --output "%~dp0artifacts";
-
+dotnet build --configuration Release --force /property:Version=$ENETVER && \
+dotnet pack /p:Version=$ENETVER --configuration Release --force --output "artifacts";
 cd ../;
-
 echo "Restore & Build CLI"
 cd ElectronNET.CLI && \
 dotnet restore && \
-dotnet build --configuration Release --force /property:Version=5.22.12 && \
-dotnet pack /p:Version=5.22.12 --configuration Release --force --output "%~dp0artifacts";
+dotnet build --configuration Release --force /property:Version=$ENETVER && \
+dotnet pack /p:Version=$ENETVER --configuration Release --force --output "artifacts";
